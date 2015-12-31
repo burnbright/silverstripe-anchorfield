ac4e6e237568515df300ba60e2527c85b1dec62b
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:58:16 +1300

    Converted to PSR-2

--- code/AnchorField.php
+++ code/AnchorField.php
@@ -3,16 +3,18 @@
 /**
  * Link Action is a field for creating html anchor tags.
  */
-class AnchorField extends DatalessField{
+class AnchorField extends DatalessField
+{
 
-	public function Type() {
-		return "anchor";
-	}
+    public function Type()
+    {
+        return "anchor";
+    }
 
-	public function Field($properties = array()) {
-		$this->setAttribute('href', $this->value);
-		
-		return parent::Field();
-	}
-
+    public function Field($properties = array())
+    {
+        $this->setAttribute('href', $this->value);
+        
+        return parent::Field();
+    }
 }
