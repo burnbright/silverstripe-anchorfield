f4a8cfd816d077048bb319fc5838cf082cfe6f15
Chris Tohill <chris@visualoverdose.ca>
2020-02-23 09:15:40 -0700

    Updated module for use in SilverStripe 4

--- src/AnchorField.php
+++ src/AnchorField.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace BurnBright\AnchorField;
+
+use SilverStripe\Forms\DatalessField;
+
 /**
  * Link Action is a field for creating html anchor tags.
  */
@@ -11,7 +15,7 @@ class AnchorField extends DatalessField{
 
 	public function Field($properties = array()) {
 		$this->setAttribute('href', $this->value);
-		
+
 		return parent::Field();
 	}
 
